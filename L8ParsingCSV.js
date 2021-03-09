e86a29264ec7760193b5068230ad6cc87f196ae5
caleb.faulhaber@gmail.com <caleb.faulhaber@gmail.com>
2021-03-08 22:28:23 -0800

    added csvData variable

--- L8ParsingCSV.js
+++ L8ParsingCSV.js
@@ -1,5 +1,12 @@
-const parseCSV = () => {
+let csvData = `id,firstName,lastName,email,
+1,jane,doe,wildmirror@yahoo.com,
+2,john,doe,tamepool@hotmail.com,
+3,sherlock,holmes,mag@glass.com,
+4,natalia,romanov,8legged@ninja.com,
+5,peter,quill,starlord@gmail.com,`;
 
+const parseCSV = () => {
+    
 };
 
 
